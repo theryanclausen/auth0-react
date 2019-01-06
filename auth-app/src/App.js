9586d17995c71918625b84a1c1f92e203fa63f48
theryanclausen <theryanclausen@gmail.com>
2019-01-05 20:59:10 -0700

    auth login

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -2,12 +2,34 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
+import Nav from "./Nav";
+import Auth from "./Auth/Auth";
+import Callback from "./Callback";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
   render() {
     return (
       <>
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/" component={Nav} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/profile"
+            render={props => <Profile auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+        </div>
       </>
     );
   }
